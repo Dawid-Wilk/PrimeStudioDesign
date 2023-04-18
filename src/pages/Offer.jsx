9858b556fc095979c962664e503fff23ac460a8b
Dawid-Wilk <wilkdawid.1995@gmail.com>
2023-04-18 10:45:36 +0200

    offer

--- src/pages/Offer.jsx
+++ src/pages/Offer.jsx
@@ -1,7 +1,69 @@
 export const Offer = () => {
     return(
-        <div className="home">
-            <h1>OFERTA</h1>
+    <div classNameName="offer">
+        <div className="offer-container">
+            <div className="offer-box">
+                <h2 className="offer-header">Architektura</h2>
+                <p className="offer-description">
+                    projekt indywidualny/adaptacja proejktu typowego (SWITCH), Powierzchnia ar działki, przeznaczenie budynku, ilosc pomieszczen, lokalizacja + wiadomosc
+                </p>
+                <div className="button-container">
+                    <a href="/" className="button-text">
+                        Zapytaj o wycenę
+                    </a>
+                    <a href="/" className="button-icon">
+                        <i className="fa fa-icon" aria-hidden="true">OOO</i>
+                    </a>
+                </div>
+            </div>
+            <div className="offer-box">
+                <h2 className="offer-header">Aranżacje Wnętrz</h2>
+                <p className="offer-description">
+                    metry kwardatowe, przeznacznie pomieszczen, iteresujacy czas realizacji, lokalizacja + wiadomosc + inspiracje : zalczaniki
+                </p>
+                <div className="button-container">
+                    <a href="/" className="button-text">
+                        Zapytaj o wycenę
+                    </a>
+                    <a href="/" className="button-icon">
+                        <i className="fa fa-icon" aria-hidden="true">OOO</i>
+                    </a>
+                </div>
+            </div>
+            <div className="offer-box">
+                <h2 className="offer-header">Od A Do Z</h2>
+                <p className="offer-description">
+                    lokalizacja dzialki, przeznaczenie budynku, powierzchnia dzialki, przewidywana powierzchnia budynku + wiadomosc 
+                </p>
+                <div className="button-container">
+                    <a href="/" className="button-text">
+                        Zapytaj o wycenę
+                    </a>
+                    <a href="/" className="button-icon">
+                        <i className="fa fa-icon" aria-hidden="true">OOO</i>
+                    </a>
+                </div>
+            </div>
         </div>
+        <div>
+            <h1 className="text-center">Jak wygląda współpraca?</h1>
+            <div className="coop-container">
+                <div className="box">
+                    <h2 className="offer-header">Nagłówek 1</h2>
+                    <p className="offer-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, reprehenderit.</p>
+                </div>
+                <div className="box">
+                    <h2>Nagłówek 2</h2>
+                    <p className="offer-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos nemo nihil minima, dolor doloremque incidunt non exercitationem tenetur eveniet cum!</p>
+                </div>
+                <div className="box">
+                    <h2>Nagłówek 3</h2>
+                    <p className="offer-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi dolorem porro numquam quaerat doloribus vero possimus, nostrum adipisci.</p>
+                </div>
+            </div>
+            <h1 className="text-center">...dodatkowo</h1>
+        </div>
+        
+    </div>
     )
 }
