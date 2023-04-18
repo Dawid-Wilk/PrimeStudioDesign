c2afb07bd272b39473dc0db0bbc7f59ca98435de
Dawid-Wilk <wilkdawid.1995@gmail.com>
2023-04-18 10:45:27 +0200

    portfolio

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,7 +1,23 @@
+import { Link } from "react-router-dom"
+
 export const Porftolio = () => {
     return(
-        <div className="home">
-            <h1>PORTFOLIO</h1>
+        <div className="portfolio">
+            <h1 className="portfolio-header">Nasze projekty</h1>
+            <div className="portfolio-container">
+                <div className="portfolio-image-container">
+                    <Link to={"/"}>
+                        <img className="image-container-img" src="./images/gory.jpeg" alt="Obraz 1" />
+                        <p className="image-container-description">Tekst na obrazie 1</p>
+                    </Link>
+                </div>
+                <div className="portfolio-image-container">
+                    <Link to={"/"}>
+                        <img className="image-container-img" src="./images/gory.jpeg" alt="Obraz 2" />
+                        <p className="image-container-description">Tekst na obrazie 2</p>
+                    </Link>
+                </div>
+            </div>
         </div>
     )
 }
