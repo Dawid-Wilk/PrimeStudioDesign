8d91256a8897f4bc440a9d0451248b1d0732034d
Dawid-Wilk <wilkdawid.1995@gmail.com>
2023-04-18 10:46:24 +0200

    slider + styles

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -9,25 +9,59 @@ export const Slider = () => {
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="./images/gory.jpeg"
+          src="./images/01.jpg"
           alt="First"
         />
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="./images/plaza.jpeg"
+          src="./images/02.jpg"
           alt="Second"
         />
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="./images/wulkan.jpeg"
+          src="./images/03.jpg"
           alt="Third"
         />
       </Carousel.Item>
-      {/* Dodaj kolejne slajdy tutaj */}
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="./images/04.jpg"
+          alt="Third"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="./images/05.jpg"
+          alt="Third"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="./images/06.jpg"
+          alt="Third"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="./images/07.jpg"
+          alt="Third"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="./images/08.jpg"
+          alt="Third"
+        />
+      </Carousel.Item>
     </Carousel>
   );
 }
