8586e24f656d08ccf7660eb04f6630d5007441de
Dawid-Wilk <wilkdawid.1995@gmail.com>
2023-04-19 21:43:43 +0200

    font + styles

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,11 +9,11 @@ export const HomePage = () => {
                     <img src="./images/logo.png" alt="Architekt Przemysław Broniec" />
                 </div>
                 <div className="about-description-container">
-                    <h1>O MNIE</h1>
+                    <h1 className="about-header">Kilka Słów O Mnie</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, tempora.</p>
                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit enim fugiat, provident voluptatem omnis recusandae expedita consectetur vel reprehenderit maxime.</p>
                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid dolorem distinctio laboriosam sequi. Repellendus, libero unde nulla vero odio enim, dolore, minima vel repudiandae dignissimos error. Nostrum dolores eius minima.</p>
-                    <button onClick={()=> console.log('DZWON')}>ZADZOWN / NAPISZ</button>
+                    <button type="button" class="btn btn-dark">Zadzwon / Napisz</button>
                 </div>
             </div>
         </>
